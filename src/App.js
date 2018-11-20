09d577bdbb86cd1c42de48e640e78f548161c3ed
Jo Minji <mingi2687@gamil.com>
2018-11-20 16:19:53 +0900

    새 글 작성 기능

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 
 import LoginForm from './components/LoginForm'
 import RegisterForm from './components/RegisterForm'
-import PostList from "./components/PostList";
+import PostList from "./components/PostList"
 import PostDetail from './components/PostDetail'
-
+import NewPostForm from './components/NewPostForm'
 
 //로그인홈에 회원가입 버튼 만들기
 //회원가입 버튼 킅릭하면 회원가입 폼 보여주기
@@ -15,6 +15,7 @@ class App extends Component {
     super(props)
     //page ==='register'--> 회원가입 페이지
     //page === 'past-list' ->게시물 목록 페이지
+    //page === 'new-post-form'-->새 글 쓰기 페이지
     this.state={
       page:'post-list',
       //현제 보고있는 게시물의 Id s
@@ -33,6 +34,12 @@ class App extends Component {
     })
   }
 
+  handleNewPostFormPage(){
+    this.setState({
+      page:'new-post-form'
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -41,10 +48,15 @@ class App extends Component {
        ) :this.state.page ==='register' ?(
          <RegisterForm/>
        ):this.state.page ==='post-list' ?(
-         <PostList onPostDetailPage={ postId => this.handlePostDetilPage(postId)}/>
+         <PostList 
+           onPostDetailPage={ postId => this.handlePostDetilPage(postId)}
+           onNewPostFormPage={()=>this.handleNewPostFormPage()}
+         />
        ):this.state.page ==='post-detail'?(
          <PostDetail postId={this.state.postId}/>
-       ):null}
+       ):this.state.page ==='new-post-form'?(
+          <NewPostForm onPostDetailPage={postId => this.handlePostDetilPage(postId)}/>
+       ) :null}
       </div>
     );
   }
---
+++ src/components/NewPostForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import api from '../api';
+
+export default class NewPostForm extends Component {
+
+ async handleSubmit(e){
+   e.preventDefault()
+   const title = e.target.elements.title.value
+   const body = e.target.elements.body.value
+   const res = await api.post('/posts',{
+     title,
+     body
+   })
+   this.props.onPostDetailPage(res.data.id)
+   //TODO:생성된 게시물 보여주기
+ }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={e=>this.handleSubmit(e)}>
+            <input type="text" name="title"/>
+            <textarea name="body" cols="30" rows="10"></textarea>
+            <button>전송</button>
+        </form>
+      </div>
+    )
+  }
+}
